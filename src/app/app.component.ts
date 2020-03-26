f01a0b80ecf3a0195fd28e53ea2b5244c101d781
pashkovdv <pashkovdv@mail.ru>
2020-03-26 12:10:42 +0300

    some bugs fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { timer } from 'rxjs';
 
 import question from './questions.js'
 
+const timeToAnswer = 10;
+const numberOfQuestions = 100;
+
 function setStorage (n: string, o: object) {
   localStorage.setItem(n, JSON.stringify(o) )
 }
@@ -11,7 +14,15 @@ function getStorage (n: string) {
   return JSON.parse( localStorage.getItem(n) );
 }
 
-const timeToAnswer = 10;
+function makeStatText( stat: any ){
+  let txt;
+  txt = stat.left + " " + stat.correct + "/" + stat.all + " ";
+  txt += (stat.all > 0) ? Math.round (numberOfQuestions*stat.correct/stat.all) : "0";
+  txt += "%";
+  return txt;
+}
+
+
 
 @Component({
   selector: 'app-root',
@@ -41,7 +52,7 @@ export class AppComponent {
       setStorage('saved_question', this.task)
       if (!this.task.correctFound && !this.stat.end){
         this.stat.left++
-        this.stat.text = this.stat.left + " " + this.stat.correct + "/" + this.stat.all + " " + Math.round (100*this.stat.correct/this.stat.all) + "%";
+        this.stat.text = makeStatText(this.stat);
         setStorage('saved_stat', this.stat)
       }
     }
@@ -50,7 +61,7 @@ export class AppComponent {
   stat: any = {
     correct: 0,
     all: 0,
-    left: 100,
+    left: numberOfQuestions,
     end: true,
     text: '0 0/0 0%'
   };
@@ -94,9 +105,15 @@ export class AppComponent {
     this.task = tempQuestion;
 
     let saved_stat = getStorage('saved_stat')
+    
     if (saved_stat === null ){
       setStorage('saved_stat', this.stat)
     } else {
+      if ( !saved_stat.end ) {
+        saved_stat.left = saved_stat.left + 1;
+        saved_stat.text = makeStatText(saved_stat);
+        setStorage('saved_stat', saved_stat)
+      }
       this.stat = saved_stat;
     }
 
@@ -105,9 +122,9 @@ export class AppComponent {
   statReset() {
     this.stat.correct = 0,
     this.stat.all = 0,
-    this.stat.left = 100;
+    this.stat.left = numberOfQuestions;
     this.stat.end = false,
-    this.stat.text = '100 0/0 0%'
+    this.stat.text = numberOfQuestions + ' 0/0 0%'
 
     this.timeLeft = timeToAnswer;
   }
@@ -148,7 +165,7 @@ export class AppComponent {
       this.histOfTrain[op][i][j] = Math.round (0.7 * this.histOfTrain[op][i][j]);
     }
 
-    this.stat.text = this.stat.left + " " + this.stat.correct + "/" + this.stat.all + " " + Math.round (100*this.stat.correct/this.stat.all) + "%";
+    this.stat.text = makeStatText(this.stat);
     if (this.stat.left <= 0){ 
       this.stat.end = true;
       setStorage('saved_stat', this.stat)
@@ -165,6 +182,17 @@ export class AppComponent {
     this.task = question(this.histOfTrain, this.hardness);
 
     setStorage('saved_question', this.task)
+  }
+
+  paramlog() {
+    let at = [];
+    at.push("--------------task---------------")
+    for (let v in this.task) at.push(v + " = " + this.task[v])
+    at.push("--------------stat---------------")
+    for (let v in this.stat) at.push(v + " = " + this.stat[v])
+    at.push("--------------timeLeft---------------")
+    at.push("timeLeft = " + this.timeLeft)
+    return at;
   }
 
 }
